69ce1e2bfad638d19d5bad4562a8e190a4ad4151
janiejestemja <janiejestemja@web.de>
2025-12-18 14:41:08 +0100

    remove typed integers and fallible iterator implementations

--- src/values/core_values/integer/typed_integer.rs
+++ src/values/core_values/integer/typed_integer.rs
@@ -64,7 +64,7 @@ pub enum IntegerTypeVariant {
     // TODO #610: ubig?
 }
 
-#[derive(Debug, Clone, Eq, PartialOrd)]
+#[derive(Debug, Clone, Eq)]
 pub enum TypedInteger {
     IBig(Integer),
     I8(i8),
