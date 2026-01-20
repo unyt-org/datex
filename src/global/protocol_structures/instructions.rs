unyt-org/datex-core

added ranges to compiler? :eyes:

src/global/protocol_structures/instructions.rs
@@ -30,6 +30,8 @@ pub enum Instruction {
     // big integers
     BigInteger(IntegerData),
 
+    Range(RangeData),
+
     Endpoint(Endpoint),
 
     DecimalF32(Float32Data),
@@ -124,6 +126,10 @@ impl Display for Instruction {
             Instruction::UInt64(data) => core::write!(f, "UINT_64 {}", data.0),
             Instruction::UInt128(data) => {
                 core::write!(f, "UINT_128 {}", data.0)
+            }
+
+            Instruction::Range(data) => {
+                core::write!(f, "RANGE {} {}", data.start, data.end)
             }
 
             Instruction::Apply(count) => {
@@ -460,4 +466,11 @@ pub struct ExecutionBlockData {
 #[brw(little)]
 pub struct ApplyData {
     pub arg_count: u16,
+}
+
+#[derive(BinRead, BinWrite, Clone, Debug, PartialEq)]
+#[brw(little)]
+pub struct RangeData {
+    pub start: Integer,
+    pub end: Integer,
 }

unyt-org/datex

src/global/protocol_structures/instructions.rs
@@ -68,6 +68,7 @@ pub enum RegularInstruction {
 
     // default integer
     Integer(IntegerData),
+    Range(RangeData),
 
     Endpoint(Endpoint),
 
@@ -196,11 +197,12 @@ impl Display for RegularInstruction {
             RegularInstruction::UInt128(data) => {
                 core::write!(f, "UINT_128 {}", data.0)
             }
-
+            RegularInstruction::Range(data) => {
+                core::write!(f, "RANGE {} {}", data.start, data.end)
+            }
             RegularInstruction::Apply(count) => {
                 core::write!(f, "APPLY {}", count.arg_count)
             }
-
             RegularInstruction::BigInteger(data) => {
                 core::write!(f, "BIG_INTEGER {}", data.0)
             }
@@ -684,4 +686,11 @@ pub struct TypeReferenceData {
 #[brw(little)]
 pub struct TypeMetadata {
     pub mutability: TypeMutabilityCode,
+}
+
+#[derive(BinRead, BinWrite, Clone, Debug, PartialEq)]
+#[brw(little)]
+pub struct RangeData {
+    pub start: Integer,
+    pub end: Integer,
 }