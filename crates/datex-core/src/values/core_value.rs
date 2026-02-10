f1dfe18a04c16daae7b5a796710544d13d35a778
janiejestemja <janiejestemja@web.de>
2026-02-10 11:50:07 +0100

    add unit test for range to string from core value

--- crates/datex-core/src/values/core_value.rs
+++ crates/datex-core/src/values/core_value.rs
@@ -21,6 +21,7 @@ use crate::{
             },
             list::List,
             map::Map,
+            range::Range,
             text::Text,
             r#type::Type,
         },
@@ -31,8 +32,6 @@ use core::{
     fmt::{Display, Formatter},
     ops::{Add, AddAssign, Neg, Not, Sub},
 };
-
-use crate::values::core_values::range::Range;
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash, FromCoreValue)]
 pub enum CoreValue {
@@ -810,5 +809,17 @@ mod tests {
         let endpoint: Endpoint = CoreValue::from("@test").try_into().unwrap();
         debug!("Endpoint: {endpoint}");
         assert_eq!(endpoint.to_string(), "@test");
+    }
+
+    #[test]
+    pub fn range_from_core() {
+        assert_eq!(
+            CoreValue::from(Range {
+                start: Box::new(Integer::from(11).into()),
+                end: Box::new(Integer::from(13).into())
+            })
+            .to_string(),
+            "11..13"
+        );
     }
 }
