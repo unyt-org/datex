355619ff80b82b8d140dcc2dda489eac8539507f
Benedikt Strehle <benedikt@unyt.org>
2026-02-17 17:45:18 +0100

    work on tests

--- crates/datex-core/src/network/com_hub/com_hub_interface.rs
+++ crates/datex-core/src/network/com_hub/com_hub_interface.rs
@@ -134,123 +134,21 @@ impl ComHub {
 
 #[cfg(test)]
 mod tests {
-    use alloc::boxed::Box;
-    use alloc::rc::Rc;
     use alloc::string::ToString;
-    use alloc::sync::Arc;
     use alloc::vec::Vec;
     use core::future::join;
-    use core::pin::Pin;
-    use futures_util::lock::Mutex;
-    use log::info;
-    use crate::channel::mpsc::{create_unbounded_channel, UnboundedReceiver, UnboundedSender};
+    use tokio::task::{LocalSet};
+    use crate::channel::mpsc::{create_unbounded_channel, UnboundedSender};
     use crate::global::dxb_block::{DXBBlock, IncomingSection};
+    use crate::global::protocol_structures::routing_header::SignatureType;
     use crate::network::com_hub::{ComHub, InterfacePriority};
     use crate::network::com_hub::metadata::ComHubMetadata;
+    use crate::network::com_hub::test_utils::{get_coupled_com_hubs, run_with_coupled_com_hubs};
     use crate::network::com_interfaces::com_interface::ComInterfaceUUID;
     use crate::network::com_interfaces::com_interface::factory::{ComInterfaceConfiguration, SendCallback, SendSuccess, SocketConfiguration, SocketProperties};
     use crate::network::com_interfaces::com_interface::properties::{ComInterfaceProperties, InterfaceDirection};
-    use crate::task::timeout;
+    use crate::task::{timeout};
     use crate::values::core_values::endpoint::Endpoint;
-
-    struct ComHubPeer {
-        com_hub: Rc<ComHub>,
-        task_future: Pin<Box<dyn Future<Output = ()>>>,
-        incoming_sections_receiver: UnboundedReceiver<IncomingSection>,
-        com_interface_uuid: ComInterfaceUUID,
-        com_interface_properties: Rc<ComInterfaceProperties>,
-    }
-
-    /// Creates two bidirectionally coupled ComInterfaceConfigurations for testing purposes.
-    fn get_bidirectionally_coupled_com_hubs() -> (ComHubPeer, ComHubPeer) {
-
-        let (incoming_sections_sender_a, incoming_sections_receiver_a) =
-            create_unbounded_channel::<IncomingSection>();
-        let (com_hub_a, task_future_a) = ComHub::create(Endpoint::new("@test-a"), incoming_sections_sender_a);
-
-        let (incoming_sections_sender_b, incoming_sections_receiver_b) =
-            create_unbounded_channel::<IncomingSection>();
-        let (com_hub_b, task_future_b) = ComHub::create(Endpoint::new("@test-b"), incoming_sections_sender_b);
-
-
-        let (send_a_to_b, mut receive_a_to_b) = create_unbounded_channel::<DXBBlock>();
-        let send_a_to_b = Arc::new(Mutex::new(send_a_to_b));
-        let (send_b_to_a, mut receive_b_to_a) = create_unbounded_channel::<DXBBlock>();
-        let send_b_to_a = Arc::new(Mutex::new(send_b_to_a));
-
-
-        let config_b = ComInterfaceConfiguration::new_single_socket(
-            ComInterfaceProperties {
-                interface_type: "test-interface".to_string(),
-                direction: InterfaceDirection::InOut,
-                ..Default::default()
-            },
-            SocketConfiguration::new_in_out(
-                SocketProperties::new(InterfaceDirection::InOut, 1),
-                async gen move {
-                    while let Some(block) = receive_a_to_b.next().await {
-                        yield Ok(block.to_bytes())
-                    }
-                },
-                SendCallback::new_async(move |block| {
-                    let send_b_to_a = send_b_to_a.clone();
-                    async move {
-                        send_b_to_a.lock().await.start_send(block).unwrap();
-                        Ok(())
-                    }
-                })
-            )
-        );
-
-        let config_a = ComInterfaceConfiguration::new_single_socket(
-            ComInterfaceProperties {
-                interface_type: "test_interface".to_string(),
-                direction: InterfaceDirection::InOut,
-                ..Default::default()
-            },
-            SocketConfiguration::new_in_out(
-                SocketProperties::new(InterfaceDirection::InOut, 1),
-                async gen move {
-                    while let Some(block) = receive_b_to_a.next().await {
-                        yield Ok(block.to_bytes())
-                    }
-                },
-                SendCallback::new_async(move |block| {
-                    let send_a_to_b = send_a_to_b.clone();
-                    async move {
-                        send_a_to_b.lock().await.start_send(block).unwrap();
-                        Ok(())
-                    }
-                })
-            )
-        );
-
-        let com_interface_uuid_a = config_a.uuid();
-        let com_interface_uuid_b = config_b.uuid();
-        let com_interface_properties_a = config_a.properties.clone();
-        let com_interface_properties_b = config_b.properties.clone();
-
-        com_hub_a.clone().add_interface_from_configuration(config_a, InterfacePriority::default()).unwrap();
-        com_hub_b.clone().add_interface_from_configuration(config_b, InterfacePriority::default()).unwrap();
-
-
-        (
-            ComHubPeer {
-                com_hub: com_hub_a,
-                task_future: Box::pin(task_future_a),
-                incoming_sections_receiver: incoming_sections_receiver_a,
-                com_interface_uuid: com_interface_uuid_a,
-                com_interface_properties: com_interface_properties_a,
-            },
-            ComHubPeer {
-                com_hub: com_hub_b,
-                task_future: Box::pin(task_future_b),
-                incoming_sections_receiver: incoming_sections_receiver_b,
-                com_interface_uuid: com_interface_uuid_b,
-                com_interface_properties: com_interface_properties_b,
-            }
-        )
-    }
 
     fn get_metadata_sockets(
         com_hub_metadata: ComHubMetadata,
@@ -308,7 +206,7 @@ mod tests {
     #[tokio::test]
     #[cfg(any(feature = "std", feature = "allow_unsigned_blocks"))]
     async fn test_connected_interfaces() {
-        let (peer_a, peer_b) = get_bidirectionally_coupled_com_hubs();
+        let (peer_a, peer_b) = get_coupled_com_hubs();
 
         // run task futures for 10ms to allow sockets to connect
         let _ = timeout(core::time::Duration::from_millis(10), join!(
@@ -325,5 +223,31 @@ mod tests {
 
         assert_eq!(sockets_a[0].0.as_ref().unwrap().to_string(), "@test-b");
         assert_eq!(sockets_b[0].0.as_ref().unwrap().to_string(), "@test-a");
+    }
+
+    #[tokio::test]
+    #[cfg(feature = "std")]
+    async fn test_interfaces_send_block() {
+        flexi_logger::init();
+
+        run_with_coupled_com_hubs(|peer_a, mut peer_b| async move {
+            // create block to send from A to B
+            let block_a_to_b_body = [1, 2, 3];
+            let mut block_a_to_b = DXBBlock::new_with_body(&block_a_to_b_body);
+            block_a_to_b.set_receivers(vec![peer_b.com_hub.endpoint.clone()]);
+            block_a_to_b.routing_header.flags.set_signature_type(SignatureType::Unencrypted);
+
+            // send block from A to B
+            peer_a.com_hub.send_own_block_async(block_a_to_b).await.unwrap();
+
+            // receive block on B
+            let section = peer_b.incoming_sections_receiver.next().await.unwrap();
+            match section {
+                IncomingSection::SingleBlock((Some(block), _)) => {
+                    assert_eq!(block.body, block_a_to_b_body);
+                },
+                _ => panic!("Expected block section")
+            }
+        }).await;
     }
 }
---
+++ crates/datex-core/src/network/com_hub/test_utils.rs
@@ -0,0 +1,159 @@
+use crate::prelude::*;
+use alloc::rc::Rc;
+use core::pin::Pin;
+use alloc::sync::Arc;
+use futures_util::lock::Mutex;
+use tokio::task::{spawn_local, yield_now, LocalSet};
+use crate::channel::mpsc::{create_unbounded_channel, UnboundedReceiver};
+use crate::global::dxb_block::{DXBBlock, IncomingSection};
+use crate::network::com_hub::{ComHub, InterfacePriority};
+use crate::network::com_interfaces::com_interface::ComInterfaceUUID;
+use crate::network::com_interfaces::com_interface::factory::{ComInterfaceConfiguration, SendCallback, SocketConfiguration, SocketProperties};
+use crate::network::com_interfaces::com_interface::properties::{ComInterfaceProperties, InterfaceDirection};
+use crate::task::sleep;
+use crate::values::core_values::endpoint::Endpoint;
+
+pub struct ComHubPeerWithFuture {
+    pub com_hub: Rc<ComHub>,
+    pub task_future: Pin<Box<dyn Future<Output = ()>>>,
+    pub incoming_sections_receiver: UnboundedReceiver<IncomingSection>,
+    pub com_interface_uuid: ComInterfaceUUID,
+    pub com_interface_properties: Rc<ComInterfaceProperties>,
+}
+
+pub struct ComHubPeer {
+    pub com_hub: Rc<ComHub>,
+    pub incoming_sections_receiver: UnboundedReceiver<IncomingSection>,
+    pub com_interface_uuid: ComInterfaceUUID,
+    pub com_interface_properties: Rc<ComInterfaceProperties>,
+}
+
+impl ComHubPeerWithFuture {
+    fn split(self) -> (ComHubPeer, Pin<Box<dyn Future<Output = ()>>>) {
+        (
+            ComHubPeer {
+                com_hub: self.com_hub,
+                incoming_sections_receiver: self.incoming_sections_receiver,
+                com_interface_uuid: self.com_interface_uuid,
+                com_interface_properties: self.com_interface_properties,
+            },
+            self.task_future
+        )
+    }
+}
+
+/// Creates two bidirectionally coupled ComInterfaceConfigurations for testing purposes.
+pub fn get_coupled_com_hubs() -> (ComHubPeerWithFuture, ComHubPeerWithFuture) {
+
+    let (incoming_sections_sender_a, incoming_sections_receiver_a) =
+        create_unbounded_channel::<IncomingSection>();
+    let (com_hub_a, task_future_a) = ComHub::create(Endpoint::new("@test-a"), incoming_sections_sender_a);
+
+    let (incoming_sections_sender_b, incoming_sections_receiver_b) =
+        create_unbounded_channel::<IncomingSection>();
+    let (com_hub_b, task_future_b) = ComHub::create(Endpoint::new("@test-b"), incoming_sections_sender_b);
+
+
+    let (send_a_to_b, mut receive_a_to_b) = create_unbounded_channel::<DXBBlock>();
+    let send_a_to_b = Arc::new(Mutex::new(send_a_to_b));
+    let (send_b_to_a, mut receive_b_to_a) = create_unbounded_channel::<DXBBlock>();
+    let send_b_to_a = Arc::new(Mutex::new(send_b_to_a));
+
+
+    let config_b = ComInterfaceConfiguration::new_single_socket(
+        ComInterfaceProperties {
+            interface_type: "test-interface".to_string(),
+            direction: InterfaceDirection::InOut,
+            ..Default::default()
+        },
+        SocketConfiguration::new_in_out(
+            SocketProperties::new(InterfaceDirection::InOut, 1),
+            async gen move {
+                while let Some(block) = receive_a_to_b.next().await {
+                    yield Ok(block.to_bytes())
+                }
+            },
+            SendCallback::new_async(move |block| {
+                let send_b_to_a = send_b_to_a.clone();
+                async move {
+                    send_b_to_a.lock().await.start_send(block).unwrap();
+                    Ok(())
+                }
+            })
+        )
+    );
+
+    let config_a = ComInterfaceConfiguration::new_single_socket(
+        ComInterfaceProperties {
+            interface_type: "test_interface".to_string(),
+            direction: InterfaceDirection::InOut,
+            ..Default::default()
+        },
+        SocketConfiguration::new_in_out(
+            SocketProperties::new(InterfaceDirection::InOut, 1),
+            async gen move {
+                while let Some(block) = receive_b_to_a.next().await {
+                    yield Ok(block.to_bytes())
+                }
+            },
+            SendCallback::new_async(move |block| {
+                let send_a_to_b = send_a_to_b.clone();
+                async move {
+                    send_a_to_b.lock().await.start_send(block).unwrap();
+                    Ok(())
+                }
+            })
+        )
+    );
+
+    let com_interface_uuid_a = config_a.uuid();
+    let com_interface_uuid_b = config_b.uuid();
+    let com_interface_properties_a = config_a.properties.clone();
+    let com_interface_properties_b = config_b.properties.clone();
+
+    com_hub_a.clone().add_interface_from_configuration(config_a, InterfacePriority::default()).unwrap();
+    com_hub_b.clone().add_interface_from_configuration(config_b, InterfacePriority::default()).unwrap();
+
+
+    (
+        ComHubPeerWithFuture {
+            com_hub: com_hub_a,
+            task_future: Box::pin(task_future_a),
+            incoming_sections_receiver: incoming_sections_receiver_a,
+            com_interface_uuid: com_interface_uuid_a,
+            com_interface_properties: com_interface_properties_a,
+        },
+        ComHubPeerWithFuture {
+            com_hub: com_hub_b,
+            task_future: Box::pin(task_future_b),
+            incoming_sections_receiver: incoming_sections_receiver_b,
+            com_interface_uuid: com_interface_uuid_b,
+            com_interface_properties: com_interface_properties_b,
+        }
+    )
+}
+
+pub async fn run_with_coupled_com_hubs<F, Fut>(test: F) -> Fut::Output
+where
+    F: FnOnce(ComHubPeer, ComHubPeer) -> Fut,
+    Fut: Future,
+{
+    let local = LocalSet::new();
+    local
+        .run_until(async {
+            let (peer_a_with_future, peer_b_with_future) = get_coupled_com_hubs();
+
+            let (peer_a, peer_a_future) = peer_a_with_future.split();
+            let (peer_b, peer_b_future) = peer_b_with_future.split();
+
+            // run task futures in background
+            spawn_local(peer_a_future);
+            spawn_local(peer_b_future);
+
+            // allow sockets to connect
+            yield_now().await;
+
+            test(peer_a, peer_b).await
+        }
+        ).await
+}
