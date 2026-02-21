0c530c79852d8476490d629fd4a20d41ca8c11e0
Benedikt Strehle <benedikt@unyt.org>
2026-02-21 22:33:01 +0100

    fix interface creation order

--- crates/datex-core/src/runtime/internal.rs
+++ crates/datex-core/src/runtime/internal.rs
@@ -108,10 +108,8 @@ impl RuntimeInternal {
     /// Performs asynchronous initialization of the runtime
     pub(crate) async fn init_async(&self) {
         // create local loopback interface and other configured interfaces
-        join(
-            self.init_local_loopback_interface(),
-            self.create_configured_interfaces()
-        ).await;
+        self.init_local_loopback_interface().await;
+        self.create_configured_interfaces().await;
     }
 
     #[cfg(feature = "compiler")]
