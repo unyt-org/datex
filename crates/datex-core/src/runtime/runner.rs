1b85355faeb95aa221ab7c1214574e45b817428d
Benedikt Strehle <benedikt@unyt.org>
2026-02-21 21:03:03 +0100

    add interface socket ready signal

--- crates/datex-core/src/runtime/runner.rs
+++ crates/datex-core/src/runtime/runner.rs
@@ -5,7 +5,9 @@ use core::cell::RefCell;
 use crate::collections::HashMap;
 use core::pin::Pin;
 use async_select::select;
+use futures::channel::oneshot;
 use futures_util::{future, join};
+use futures_util::future::{join, Join};
 use log::info;
 use crate::channel::mpsc::create_unbounded_channel;
 use crate::global::dxb_block::IncomingSection;
@@ -54,7 +56,6 @@ impl RuntimeRunner {
                 execution_contexts: RefCell::new(HashMap::new()),
             }),
         };
-        runtime.init_local_loopback_interface();
 
         let runtime_internal = runtime.internal.clone();
 
@@ -85,17 +86,14 @@ impl RuntimeRunner {
     where
         AppFuture: Future<Output = AppReturn>,
     {
-        // initialize the runtime
-        self.runtime.internal.init_async().await;
-        // start the app logic
-        let app_future = app_logic(self.runtime);
+        let (runtime_future, app_future) = self.create_runtime_and_app_future(app_logic);
 
         // run until the app logic completes
         select! {
-            _ = self.task_future.fuse() => {
+            _ = runtime_future => {
                 unreachable!("Runtime task future exited unexpectedly");
             },
-            exit_value = app_future.fuse() => {
+            exit_value = app_future => {
                 exit_value
             },
         }
@@ -109,13 +107,71 @@ impl RuntimeRunner {
     where
         AppFuture: Future<Output = AppReturn>,
     {
-        // initialize the runtime
-        self.runtime.internal.init_async().await;
-        // start the app logic
-        let app_future = app_logic(self.runtime);
+        let (runtime_future, app_future) = self.create_runtime_and_app_future(app_logic);
 
         // run indefinitely (runtime future should never exit)
-        future::join(self.task_future, app_future).await;
+        future::join(runtime_future, app_future).await;
         unreachable!("Both runtime and app logic futures exited unexpectedly");
+    }
+
+    /// Creates the runtime future and the app future, ensuring that the app logic starts executing only after the runtime has completed its initialization.
+    fn create_runtime_and_app_future<AppReturn, AppFuture>(
+        self,
+        app_logic: impl FnOnce(Runtime) -> AppFuture,
+    ) -> (Join<impl Future<Output = ()>, impl Future<Output = ()>>, impl Future<Output = AppReturn>)
+    where
+        AppFuture: Future<Output = AppReturn>
+    {
+        let (init_ready_sender, init_ready_receiver) = oneshot::channel();
+        let runtime = self.runtime.clone();
+        let runtime_future = join(
+            // initialize the runtime
+            async move {
+                runtime.internal.init_async().await;
+                init_ready_sender.send(()).unwrap();
+            },
+            // run tasks
+            self.task_future
+        );
+
+        // start the app logic
+        let app_future = async move {
+            // wait for runtime initialization to complete before starting app logic
+            init_ready_receiver.await.unwrap();
+            app_logic(self.runtime).await
+        };
+
+        (runtime_future, app_future)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_runtime_runner() {
+        let runner = RuntimeRunner::new(RuntimeConfig::default());
+        let mut runtime = None;
+        runner.run(async |runtime_inner| {
+            runtime = Some(runtime_inner);
+        }).await;
+
+        assert!(runtime.is_some());
+
+        // check if local loopback interface was fully initialized and is present in the com hub
+        let com_hub = runtime.unwrap().com_hub();
+        let interface_map = com_hub.interfaces_manager().interfaces.borrow();
+        let local_loopback_interface = interface_map.iter().find(|(uuid, interface)| {
+            interface.properties.interface_type == "local"
+        });
+        let (local_loopback_interface_uuid, _) = local_loopback_interface.expect("Local loopback interface not found in com hub");
+
+        // check if socket for the local loopback interface is present in the com hub
+        let socket_map = com_hub.socket_manager().sockets.borrow();
+        let local_loopback_socket = socket_map.values().find(|socket| {
+            &socket.interface_uuid == local_loopback_interface_uuid
+        }).expect("Local loopback socket not found in com hub");
     }
 }
