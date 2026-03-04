1d12a776043167f6bc72d52da72121174f88dede
Jonas Strehle <jonas@strehles.info>
2026-03-04 20:25:09 +0100

    fix: update clippy command to use workspace flag in fmt.sh

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 cargo clippy -p datex-core --fix --features decompiler,compiler,allow_unsigned_blocks
-cargo clippy --exclude datex-core --fix
+cargo clippy --workspace --exclude datex-core --fix
 cargo fmt --all
