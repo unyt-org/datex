76a524a8f6ec167d9878e5e2273daa8d1dd6afb4
Jonas Strehle <jonas@strehles.info>
2026-03-04 20:55:49 +0100

    add full flag

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-cargo clippy -p datex-core --fix --features decompiler,compiler,allow_unsigned_blocks
+cargo clippy -p datex-core --fix --features full,allow_unsigned_blocks
 cargo clippy --workspace --exclude datex-core --fix
 cargo fmt --all
 git commit -a -m "fmt"
