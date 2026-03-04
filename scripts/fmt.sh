c68ab841b5ba6d163555741b0f5ab9537c2694e4
Jonas Strehle <jonas@strehles.info>
2026-03-04 20:26:25 +0100

    fix: add git commit command to fmt.sh for automatic formatting commits

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -3,3 +3,4 @@ set -euo pipefail
 cargo clippy -p datex-core --fix --features decompiler,compiler,allow_unsigned_blocks
 cargo clippy --workspace --exclude datex-core --fix
 cargo fmt --all
+git commit -a -m "fmt"
