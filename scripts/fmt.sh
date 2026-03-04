2667c9e3ed12d25eaa1d00a56a7a56690b695b06
Jonas Strehle <jonas@strehles.info>
2026-03-04 20:24:01 +0100

    add scripts

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 cargo fmt --all
+cargo clippy -p datex-core --fix --features decompiler,compiler,allow_unsigned_blocks
+cargo clippy --exclude datex-core --fix
