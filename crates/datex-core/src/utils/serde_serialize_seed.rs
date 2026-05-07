66c57ccf8b8855b809259e5e2735a4be0805bc2a
Jonas Strehle <jonas@strehles.info>
2026-05-07 17:04:50 +0200

    no std fixes

--- crates/datex-core/src/utils/serde_serialize_seed.rs
+++ crates/datex-core/src/utils/serde_serialize_seed.rs
@@ -1,38 +1,55 @@
 /// Implementation of https://docs.rs/serde-serialize-seed/latest/serde_serialize_seed/, but with &mut self
-use core::fmt;
-use core::fmt::Formatter;
-use core::cell::UnsafeCell;
-use core::marker::PhantomData;
-use serde::{de, Deserializer, Serialize, Serializer};
-use serde::de::{DeserializeSeed, Error, SeqAccess};
-use serde::ser::{SerializeSeq, SerializeTuple};
-use crate::serde::Deserialize;
+use crate::{prelude::*, serde::Deserialize};
+use core::{cell::UnsafeCell, fmt, fmt::Formatter, marker::PhantomData};
+use serde::{
+    Deserializer, Serialize, Serializer, de,
+    de::{DeserializeSeed, Error, SeqAccess},
+    ser::{SerializeSeq, SerializeTuple},
+};
 
 pub trait SerializeSeed {
     type Value: ?Sized;
 
-    fn serialize<S: Serializer>(&mut self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error>;
+    fn serialize<S: Serializer>(
+        &mut self,
+        value: &Self::Value,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>;
 }
 
 impl<T: SerializeSeed + ?Sized> SerializeSeed for &mut T {
     type Value = T::Value;
 
-    fn serialize<S: Serializer>(&mut self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error> {
+    fn serialize<S: Serializer>(
+        &mut self,
+        value: &Self::Value,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
         (**self).serialize(value, serializer)
     }
 }
 
 #[derive(Debug)]
-pub struct ValueWithSeed<'a, Value: ?Sized, Seed>(pub &'a Value, pub UnsafeCell<Seed>);
+pub struct ValueWithSeed<'a, Value: ?Sized, Seed>(
+    pub &'a Value,
+    pub UnsafeCell<Seed>,
+);
 
-impl<'a, Value: ?Sized, Seed: SerializeSeed<Value=Value>> ValueWithSeed<'a, Value, Seed> {
+impl<'a, Value: ?Sized, Seed: SerializeSeed<Value = Value>>
+    ValueWithSeed<'a, Value, Seed>
+{
     pub fn new(value: &'a Value, seed: Seed) -> Self {
         Self(value, UnsafeCell::new(seed))
     }
 }
 
-impl<'a, Value: ?Sized, Seed: SerializeSeed<Value=Value>> Serialize for ValueWithSeed<'a, Value, Seed> {
-    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+impl<'a, Value: ?Sized, Seed: SerializeSeed<Value = Value>> Serialize
+    for ValueWithSeed<'a, Value, Seed>
+{
+    fn serialize<S: Serializer>(
+        &self,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
         // SAFETY: serde calls serialize exactly once, so no aliased &mut exists
         let seed = unsafe { &mut *self.1.get() };
         seed.serialize(self.0, serializer)
@@ -45,7 +62,11 @@ pub struct StatelessSerde<T: ?Sized>(pub PhantomData<T>);
 impl<T: Serialize + ?Sized> SerializeSeed for StatelessSerde<T> {
     type Value = T;
 
-    fn serialize<S: Serializer>(&mut self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error> {
+    fn serialize<S: Serializer>(
+        &mut self,
+        value: &Self::Value,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
         value.serialize(serializer)
     }
 }
@@ -53,7 +74,10 @@ impl<T: Serialize + ?Sized> SerializeSeed for StatelessSerde<T> {
 impl<'de, T: Deserialize<'de>> DeserializeSeed<'de> for StatelessSerde<T> {
     type Value = T;
 
-    fn deserialize<D: Deserializer<'de>>(self, deserializer: D) -> Result<Self::Value, D::Error> {
+    fn deserialize<D: Deserializer<'de>>(
+        self,
+        deserializer: D,
+    ) -> Result<Self::Value, D::Error> {
         T::deserialize(deserializer)
     }
 }
@@ -62,10 +86,17 @@ impl<'de, T: Deserialize<'de>> DeserializeSeed<'de> for StatelessSerde<T> {
 pub struct PairSerde<U, V>(pub U, pub V);
 
 impl<U: SerializeSeed, V: SerializeSeed> SerializeSeed for PairSerde<U, V>
-where U::Value: Sized, V::Value: Sized {
+where
+    U::Value: Sized,
+    V::Value: Sized,
+{
     type Value = (U::Value, V::Value);
 
-    fn serialize<S: Serializer>(&mut self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error> {
+    fn serialize<S: Serializer>(
+        &mut self,
+        value: &Self::Value,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
         let mut s = serializer.serialize_tuple(2)?;
         s.serialize_element(&ValueWithSeed::new(&value.0, &mut self.0))?;
         s.serialize_element(&ValueWithSeed::new(&value.1, &mut self.1))?;
@@ -75,8 +106,11 @@ where U::Value: Sized, V::Value: Sized {
 
 struct PairDeVisitor<U, V>(PairSerde<U, V>);
 
-impl<'de, U: DeserializeSeed<'de>, V: DeserializeSeed<'de>> de::Visitor<'de> for PairDeVisitor<U, V> where
-    U::Value: Sized, V::Value: Sized
+impl<'de, U: DeserializeSeed<'de>, V: DeserializeSeed<'de>> de::Visitor<'de>
+    for PairDeVisitor<U, V>
+where
+    U::Value: Sized,
+    V::Value: Sized,
 {
     type Value = (U::Value, V::Value);
 
@@ -84,22 +118,32 @@ impl<'de, U: DeserializeSeed<'de>, V: DeserializeSeed<'de>> de::Visitor<'de> for
         write!(f, "pair")
     }
 
-    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error> where A: SeqAccess<'de> {
-        let u = seq.next_element_seed(self.0.0)?
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: SeqAccess<'de>,
+    {
+        let u = seq
+            .next_element_seed(self.0.0)?
             .ok_or_else(|| A::Error::invalid_length(0, &"pair"))?;
-        let v = seq.next_element_seed(self.0.1)?
+        let v = seq
+            .next_element_seed(self.0.1)?
             .ok_or_else(|| A::Error::invalid_length(1, &"pair"))?;
         Ok((u, v))
     }
 }
 
-impl<'de, U: DeserializeSeed<'de>, V: DeserializeSeed<'de>> DeserializeSeed<'de> for PairSerde<U, V>
+impl<'de, U: DeserializeSeed<'de>, V: DeserializeSeed<'de>> DeserializeSeed<'de>
+    for PairSerde<U, V>
 where
-    U::Value: Sized, V::Value: Sized
+    U::Value: Sized,
+    V::Value: Sized,
 {
     type Value = (U::Value, V::Value);
 
-    fn deserialize<D: Deserializer<'de>>(self, deserializer: D) -> Result<Self::Value, D::Error> {
+    fn deserialize<D: Deserializer<'de>>(
+        self,
+        deserializer: D,
+    ) -> Result<Self::Value, D::Error> {
         deserializer.deserialize_tuple(2, PairDeVisitor(self))
     }
 }
@@ -107,18 +151,21 @@ where
 #[derive(Debug, Clone, Copy)]
 pub struct Tuple4Serde<T1, T2, T3, T4>(pub T1, pub T2, pub T3, pub T4);
 
-impl<
-    T1: SerializeSeed,
-    T2: SerializeSeed,
-    T3: SerializeSeed,
-    T4: SerializeSeed
-> SerializeSeed for Tuple4Serde<T1, T2, T3, T4>
+impl<T1: SerializeSeed, T2: SerializeSeed, T3: SerializeSeed, T4: SerializeSeed>
+    SerializeSeed for Tuple4Serde<T1, T2, T3, T4>
 where
-    T1::Value: Sized, T2::Value: Sized, T3::Value: Sized, T4::Value: Sized
+    T1::Value: Sized,
+    T2::Value: Sized,
+    T3::Value: Sized,
+    T4::Value: Sized,
 {
     type Value = (T1::Value, T2::Value, T3::Value, T4::Value);
 
-    fn serialize<S: Serializer>(&mut self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error> {
+    fn serialize<S: Serializer>(
+        &mut self,
+        value: &Self::Value,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
         let mut s = serializer.serialize_tuple(4)?;
         s.serialize_element(&ValueWithSeed::new(&value.0, &mut self.0))?;
         s.serialize_element(&ValueWithSeed::new(&value.1, &mut self.1))?;
@@ -135,9 +182,13 @@ impl<
     T1: DeserializeSeed<'de>,
     T2: DeserializeSeed<'de>,
     T3: DeserializeSeed<'de>,
-    T4: DeserializeSeed<'de>
-> de::Visitor<'de> for Tuple4DeVisitor<T1, T2, T3, T4> where
-    T1::Value: Sized, T2::Value: Sized, T3::Value: Sized, T4::Value: Sized
+    T4: DeserializeSeed<'de>,
+> de::Visitor<'de> for Tuple4DeVisitor<T1, T2, T3, T4>
+where
+    T1::Value: Sized,
+    T2::Value: Sized,
+    T3::Value: Sized,
+    T4::Value: Sized,
 {
     type Value = (T1::Value, T2::Value, T3::Value, T4::Value);
 
@@ -145,14 +196,21 @@ impl<
         write!(f, "tuple 4")
     }
 
-    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error> where A: SeqAccess<'de> {
-        let t1 = seq.next_element_seed(self.0.0)?
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: SeqAccess<'de>,
+    {
+        let t1 = seq
+            .next_element_seed(self.0.0)?
             .ok_or_else(|| A::Error::invalid_length(0, &"tuple 4"))?;
-        let t2 = seq.next_element_seed(self.0.1)?
+        let t2 = seq
+            .next_element_seed(self.0.1)?
             .ok_or_else(|| A::Error::invalid_length(1, &"tuple 4"))?;
-        let t3 = seq.next_element_seed(self.0.2)?
+        let t3 = seq
+            .next_element_seed(self.0.2)?
             .ok_or_else(|| A::Error::invalid_length(2, &"tuple 4"))?;
-        let t4 = seq.next_element_seed(self.0.3)?
+        let t4 = seq
+            .next_element_seed(self.0.3)?
             .ok_or_else(|| A::Error::invalid_length(3, &"tuple 4"))?;
         Ok((t1, t2, t3, t4))
     }
@@ -163,14 +221,20 @@ impl<
     T1: DeserializeSeed<'de>,
     T2: DeserializeSeed<'de>,
     T3: DeserializeSeed<'de>,
-    T4: DeserializeSeed<'de>
+    T4: DeserializeSeed<'de>,
 > DeserializeSeed<'de> for Tuple4Serde<T1, T2, T3, T4>
 where
-    T1::Value: Sized, T2::Value: Sized, T3::Value: Sized, T4::Value: Sized
+    T1::Value: Sized,
+    T2::Value: Sized,
+    T3::Value: Sized,
+    T4::Value: Sized,
 {
     type Value = (T1::Value, T2::Value, T3::Value, T4::Value);
 
-    fn deserialize<D: Deserializer<'de>>(self, deserializer: D) -> Result<Self::Value, D::Error> {
+    fn deserialize<D: Deserializer<'de>>(
+        self,
+        deserializer: D,
+    ) -> Result<Self::Value, D::Error> {
         deserializer.deserialize_tuple(4, Tuple4DeVisitor(self))
     }
 }
@@ -179,13 +243,20 @@ where
 pub struct VecSerde<T>(pub T);
 
 impl<T: SerializeSeed + Clone> SerializeSeed for VecSerde<T>
-where T::Value: Sized {
+where
+    T::Value: Sized,
+{
     type Value = [T::Value];
 
-    fn serialize<S: Serializer>(&mut self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error> {
+    fn serialize<S: Serializer>(
+        &mut self,
+        value: &Self::Value,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
         let mut serializer = serializer.serialize_seq(Some(value.len()))?;
         for item in value {
-            serializer.serialize_element(&ValueWithSeed::new(item, &mut self.0))?;
+            serializer
+                .serialize_element(&ValueWithSeed::new(item, &mut self.0))?;
         }
         serializer.end()
     }
@@ -193,14 +264,20 @@ where T::Value: Sized {
 
 struct VecDeVisitor<T>(VecSerde<T>);
 
-impl<'de, T: DeserializeSeed<'de> + Clone> de::Visitor<'de> for VecDeVisitor<T> where T::Value: Sized {
+impl<'de, T: DeserializeSeed<'de> + Clone> de::Visitor<'de> for VecDeVisitor<T>
+where
+    T::Value: Sized,
+{
     type Value = Vec<T::Value>;
 
     fn expecting(&self, f: &mut Formatter) -> fmt::Result {
         write!(f, "vector")
     }
 
-    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error> where A: SeqAccess<'de> {
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: SeqAccess<'de>,
+    {
         let mut vec = seq.size_hint().map_or_else(Vec::new, Vec::with_capacity);
         while let Some(f) = seq.next_element_seed(self.0.0.clone())? {
             vec.push(f);
@@ -210,10 +287,15 @@ impl<'de, T: DeserializeSeed<'de> + Clone> de::Visitor<'de> for VecDeVisitor<T> 
 }
 
 impl<'de, T: DeserializeSeed<'de> + Clone> DeserializeSeed<'de> for VecSerde<T>
-where T::Value: Sized {
+where
+    T::Value: Sized,
+{
     type Value = Vec<T::Value>;
 
-    fn deserialize<D: Deserializer<'de>>(self, deserializer: D) -> Result<Self::Value, D::Error> {
+    fn deserialize<D: Deserializer<'de>>(
+        self,
+        deserializer: D,
+    ) -> Result<Self::Value, D::Error> {
         deserializer.deserialize_seq(VecDeVisitor(self))
     }
 }
--- crates/datex-core/src/value_updates/update_data/serde_dif.rs
+++ crates/datex-core/src/value_updates/update_data/serde_dif.rs
@@ -1,23 +1,37 @@
+use crate::{
+    dif::serde_context::SerdeContext,
+    prelude::*,
+    shared_values::base_shared_value_container::observers::TransceiverId,
+    utils::serde_serialize_seed::{SerializeSeed, ValueWithSeed},
+    value_updates::update_data::{
+        AppendEntryUpdateData, DeleteEntryUpdateData, ListSpliceUpdateData,
+        ReplaceUpdateData, SetEntryUpdateData, Update, UpdateData,
+        UpdateReturn,
+    },
+    values::value_container::{ValueContainer, value_key::ValueKey},
+};
 use core::fmt;
-use serde::de::{DeserializeSeed, MapAccess, Visitor};
-use serde::{de, Deserializer, Serializer};
-use serde::de::value::MapAccessDeserializer;
-use serde::ser::{SerializeSeq, SerializeStruct};
-use crate::dif::serde_context::SerdeContext;
-use crate::shared_values::base_shared_value_container::observers::TransceiverId;
-use crate::utils::serde_serialize_seed::{SerializeSeed, ValueWithSeed};
-use crate::value_updates::update_data::{AppendEntryUpdateData, DeleteEntryUpdateData, ListSpliceUpdateData, ReplaceUpdateData, SetEntryUpdateData, Update, UpdateData, UpdateReturn};
-use crate::values::value_container::value_key::ValueKey;
-use crate::values::value_container::ValueContainer;
+use serde::{
+    Deserializer, Serializer, de,
+    de::{DeserializeSeed, MapAccess, Visitor, value::MapAccessDeserializer},
+    ser::{SerializeSeq, SerializeStruct},
+};
 
 impl<'ctx> SerializeSeed for SerdeContext<'ctx, Update> {
     type Value = Update;
 
-    fn serialize<S: Serializer>(&mut self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error> {
+    fn serialize<S: Serializer>(
+        &mut self,
+        value: &Self::Value,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
         // serialize as map with {source: <TransceiverId>, data: <UpdateData>}
         let mut state = serializer.serialize_struct("Update", 2)?;
         state.serialize_field("source", &value.source_id)?;
-        state.serialize_field("data", &ValueWithSeed::new(&value.data, self.cast::<UpdateData>()))?;
+        state.serialize_field(
+            "data",
+            &ValueWithSeed::new(&value.data, self.cast::<UpdateData>()),
+        )?;
         state.end()
     }
 }
@@ -25,8 +39,15 @@ impl<'ctx> SerializeSeed for SerdeContext<'ctx, Update> {
 impl<'de, 'ctx> DeserializeSeed<'de> for SerdeContext<'ctx, Update> {
     type Value = Update;
 
-    fn deserialize<D: Deserializer<'de>>(self, deserializer: D) -> Result<Self::Value, D::Error> {
-        deserializer.deserialize_struct("Update", &["source", "data"], UpdateVisitor { ctx: self })
+    fn deserialize<D: Deserializer<'de>>(
+        self,
+        deserializer: D,
+    ) -> Result<Self::Value, D::Error> {
+        deserializer.deserialize_struct(
+            "Update",
+            &["source", "data"],
+            UpdateVisitor { ctx: self },
+        )
     }
 }
 
@@ -41,7 +62,10 @@ impl<'de, 'ctx> Visitor<'de> for UpdateVisitor<'ctx> {
         write!(f, "a map with `source` and `data` fields")
     }
 
-    fn visit_map<A: MapAccess<'de>>(mut self, mut map: A) -> Result<Self::Value, A::Error> {
+    fn visit_map<A: MapAccess<'de>>(
+        mut self,
+        mut map: A,
+    ) -> Result<Self::Value, A::Error> {
         let mut source_id: Option<TransceiverId> = None;
         let mut data: Option<UpdateData> = None;
 
@@ -52,64 +76,84 @@ impl<'de, 'ctx> Visitor<'de> for UpdateVisitor<'ctx> {
                 }
                 "data" => {
                     // Use next_value_seed to thread your context through
-                    data = Some(map.next_value_seed(self.ctx.cast::<UpdateData>())?);
+                    data = Some(
+                        map.next_value_seed(self.ctx.cast::<UpdateData>())?,
+                    );
                 }
                 other => {
-                    return Err(de::Error::unknown_field(other, &["source", "data"]));
+                    return Err(de::Error::unknown_field(
+                        other,
+                        &["source", "data"],
+                    ));
                 }
             }
         }
 
         Ok(Update {
-            source_id: source_id.ok_or_else(|| de::Error::missing_field("source"))?,
+            source_id: source_id
+                .ok_or_else(|| de::Error::missing_field("source"))?,
             data: data.ok_or_else(|| de::Error::missing_field("data"))?,
         })
     }
 }
 
-
 impl<'ctx> SerializeSeed for SerdeContext<'ctx, UpdateData> {
     type Value = UpdateData;
 
-    fn serialize<S: Serializer>(&mut self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error> {
+    fn serialize<S: Serializer>(
+        &mut self,
+        value: &Self::Value,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
         match value {
-            UpdateData::Replace(replace_data) => {
-                self.cast::<ReplaceUpdateData>().serialize(replace_data, serializer)
-            }
-            UpdateData::SetEntry(set_entry_data) => {
-                self.cast::<SetEntryUpdateData>().serialize(set_entry_data, serializer)
-            }
-            UpdateData::DeleteEntry(delete_entry_data) => {
-                self.cast::<DeleteEntryUpdateData>().serialize(delete_entry_data, serializer)
-            }
+            UpdateData::Replace(replace_data) => self
+                .cast::<ReplaceUpdateData>()
+                .serialize(replace_data, serializer),
+            UpdateData::SetEntry(set_entry_data) => self
+                .cast::<SetEntryUpdateData>()
+                .serialize(set_entry_data, serializer),
+            UpdateData::DeleteEntry(delete_entry_data) => self
+                .cast::<DeleteEntryUpdateData>()
+                .serialize(delete_entry_data, serializer),
             UpdateData::Clear => {
                 let mut state = serializer.serialize_struct("Clear", 1)?;
                 state.serialize_field("kind", "clear")?;
                 state.end()
             }
-            UpdateData::AppendEntry(append_entry_data) => {
-                self.cast::<AppendEntryUpdateData>().serialize(append_entry_data, serializer)
-            }
-            UpdateData::ListSplice(list_splice_data) => {
-                self.cast::<ListSpliceUpdateData>().serialize(list_splice_data, serializer)
-            }
+            UpdateData::AppendEntry(append_entry_data) => self
+                .cast::<AppendEntryUpdateData>()
+                .serialize(append_entry_data, serializer),
+            UpdateData::ListSplice(list_splice_data) => self
+                .cast::<ListSpliceUpdateData>()
+                .serialize(list_splice_data, serializer),
         }
     }
 }
 
 impl<'ctx> SerializeSeed for SerdeContext<'ctx, UpdateReturn> {
     type Value = UpdateReturn;
-    fn serialize<S: Serializer>(&mut self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error> {
+    fn serialize<S: Serializer>(
+        &mut self,
+        value: &Self::Value,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
         match value {
             UpdateReturn::SingleValue(value) => {
-                let mut state = serializer.serialize_struct("SingleValue", 1)?;
-                state.serialize_field("value", &ValueWithSeed::new(value, self.cast::<ValueContainer>()))?;
+                let mut state =
+                    serializer.serialize_struct("SingleValue", 1)?;
+                state.serialize_field(
+                    "value",
+                    &ValueWithSeed::new(value, self.cast::<ValueContainer>()),
+                )?;
                 state.end()
             }
             UpdateReturn::MultipleValues(values) => {
                 let mut seq = serializer.serialize_seq(Some(values.len()))?;
                 for value in values {
-                    seq.serialize_element(&ValueWithSeed::new(value, self.cast::<ValueContainer>()))?;
+                    seq.serialize_element(&ValueWithSeed::new(
+                        value,
+                        self.cast::<ValueContainer>(),
+                    ))?;
                 }
                 seq.end()
             }
@@ -121,10 +165,17 @@ impl<'ctx> SerializeSeed for SerdeContext<'ctx, UpdateReturn> {
 impl<'ctx> SerializeSeed for SerdeContext<'ctx, ReplaceUpdateData> {
     type Value = ReplaceUpdateData;
 
-    fn serialize<S: Serializer>(&mut self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error> {
+    fn serialize<S: Serializer>(
+        &mut self,
+        value: &Self::Value,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
         let mut state = serializer.serialize_struct("ReplaceUpdateData", 2)?;
         state.serialize_field("kind", "replace")?;
-        state.serialize_field("value", &ValueWithSeed::new(&value.value, self.cast::<ValueContainer>()))?;
+        state.serialize_field(
+            "value",
+            &ValueWithSeed::new(&value.value, self.cast::<ValueContainer>()),
+        )?;
         state.end()
     }
 }
@@ -132,11 +183,21 @@ impl<'ctx> SerializeSeed for SerdeContext<'ctx, ReplaceUpdateData> {
 impl<'ctx> SerializeSeed for SerdeContext<'ctx, SetEntryUpdateData> {
     type Value = SetEntryUpdateData;
 
-    fn serialize<S: Serializer>(&mut self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error> {
+    fn serialize<S: Serializer>(
+        &mut self,
+        value: &Self::Value,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
         let mut state = serializer.serialize_struct("SetEntryUpdateData", 3)?;
         state.serialize_field("kind", "set_entry")?;
-        state.serialize_field("key", &ValueWithSeed::new(&value.key, self.cast::<ValueKey>()))?;
-        state.serialize_field("value", &ValueWithSeed::new(&value.value, self.cast::<ValueContainer>()))?;
+        state.serialize_field(
+            "key",
+            &ValueWithSeed::new(&value.key, self.cast::<ValueKey>()),
+        )?;
+        state.serialize_field(
+            "value",
+            &ValueWithSeed::new(&value.value, self.cast::<ValueContainer>()),
+        )?;
         state.end()
     }
 }
@@ -144,10 +205,18 @@ impl<'ctx> SerializeSeed for SerdeContext<'ctx, SetEntryUpdateData> {
 impl<'ctx> SerializeSeed for SerdeContext<'ctx, DeleteEntryUpdateData> {
     type Value = DeleteEntryUpdateData;
 
-    fn serialize<S: Serializer>(&mut self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error> {
-        let mut state = serializer.serialize_struct("DeleteEntryUpdateData", 2)?;
+    fn serialize<S: Serializer>(
+        &mut self,
+        value: &Self::Value,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
+        let mut state =
+            serializer.serialize_struct("DeleteEntryUpdateData", 2)?;
         state.serialize_field("kind", "delete_entry")?;
-        state.serialize_field("key", &ValueWithSeed::new(&value.key, self.cast::<ValueKey>()))?;
+        state.serialize_field(
+            "key",
+            &ValueWithSeed::new(&value.key, self.cast::<ValueKey>()),
+        )?;
         state.end()
     }
 }
@@ -155,10 +224,18 @@ impl<'ctx> SerializeSeed for SerdeContext<'ctx, DeleteEntryUpdateData> {
 impl<'ctx> SerializeSeed for SerdeContext<'ctx, AppendEntryUpdateData> {
     type Value = AppendEntryUpdateData;
 
-    fn serialize<S: Serializer>(&mut self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error> {
-        let mut state = serializer.serialize_struct("AppendEntryUpdateData", 2)?;
+    fn serialize<S: Serializer>(
+        &mut self,
+        value: &Self::Value,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
+        let mut state =
+            serializer.serialize_struct("AppendEntryUpdateData", 2)?;
         state.serialize_field("kind", "append_entry")?;
-        state.serialize_field("value", &ValueWithSeed::new(&value.value, self.cast::<ValueContainer>()))?;
+        state.serialize_field(
+            "value",
+            &ValueWithSeed::new(&value.value, self.cast::<ValueContainer>()),
+        )?;
         state.end()
     }
 }
@@ -166,12 +243,23 @@ impl<'ctx> SerializeSeed for SerdeContext<'ctx, AppendEntryUpdateData> {
 impl<'ctx> SerializeSeed for SerdeContext<'ctx, ListSpliceUpdateData> {
     type Value = ListSpliceUpdateData;
 
-    fn serialize<S: Serializer>(&mut self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error> {
-        let mut state = serializer.serialize_struct("ListSpliceUpdateData", 4)?;
+    fn serialize<S: Serializer>(
+        &mut self,
+        value: &Self::Value,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
+        let mut state =
+            serializer.serialize_struct("ListSpliceUpdateData", 4)?;
         state.serialize_field("kind", "list_splice")?;
         state.serialize_field("start", &value.start)?;
         state.serialize_field("delete_count", &value.delete_count)?;
-        state.serialize_field("items", &ValueWithSeed::new(&value.items, self.cast::<Vec<ValueContainer>>()))?;
+        state.serialize_field(
+            "items",
+            &ValueWithSeed::new(
+                &value.items,
+                self.cast::<Vec<ValueContainer>>(),
+            ),
+        )?;
         state.end()
     }
 }
@@ -179,10 +267,17 @@ impl<'ctx> SerializeSeed for SerdeContext<'ctx, ListSpliceUpdateData> {
 impl<'ctx> SerializeSeed for SerdeContext<'ctx, Vec<ValueContainer>> {
     type Value = Vec<ValueContainer>;
 
-    fn serialize<S: Serializer>(&mut self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error> {
+    fn serialize<S: Serializer>(
+        &mut self,
+        value: &Self::Value,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
         let mut seq = serializer.serialize_seq(Some(value.len()))?;
         for item in value {
-            seq.serialize_element(&ValueWithSeed::new(item, self.cast::<ValueContainer>()))?;
+            seq.serialize_element(&ValueWithSeed::new(
+                item,
+                self.cast::<ValueContainer>(),
+            ))?;
         }
         seq.end()
     }
@@ -191,12 +286,17 @@ impl<'ctx> SerializeSeed for SerdeContext<'ctx, Vec<ValueContainer>> {
 impl<'de, 'ctx> DeserializeSeed<'de> for SerdeContext<'ctx, UpdateData> {
     type Value = UpdateData;
 
-    fn deserialize<D: Deserializer<'de>>(self, deserializer: D) -> Result<Self::Value, D::Error> {
+    fn deserialize<D: Deserializer<'de>>(
+        self,
+        deserializer: D,
+    ) -> Result<Self::Value, D::Error> {
         deserializer.deserialize_map(UpdateDataVisitor { ctx: self })
     }
 }
 
-struct UpdateDataVisitor<'ctx> { ctx: SerdeContext<'ctx, UpdateData> }
+struct UpdateDataVisitor<'ctx> {
+    ctx: SerdeContext<'ctx, UpdateData>,
+}
 
 impl<'de, 'ctx> Visitor<'de> for UpdateDataVisitor<'ctx> {
     type Value = UpdateData;
@@ -205,13 +305,19 @@ impl<'de, 'ctx> Visitor<'de> for UpdateDataVisitor<'ctx> {
         write!(f, "UpdateData map with a `type` field")
     }
 
-    fn visit_map<A: MapAccess<'de>>(mut self, mut map: A) -> Result<Self::Value, A::Error> {
+    fn visit_map<A: MapAccess<'de>>(
+        mut self,
+        mut map: A,
+    ) -> Result<Self::Value, A::Error> {
         // Expect `type` as the first key
         match map.next_key::<&str>()? {
             Some("type") => {}
-            Some(other) => return Err(de::Error::custom(
-                format!("expected `type` field first, got `{}`", other)
-            )),
+            Some(other) => {
+                return Err(de::Error::custom(format!(
+                    "expected `type` field first, got `{}`",
+                    other
+                )));
+            }
             None => return Err(de::Error::missing_field("type")),
         }
 
