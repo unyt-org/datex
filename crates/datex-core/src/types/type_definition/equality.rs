803d8290ea10364f1fa7bcda5e8f34b780accf47
Jonas Strehle <jonas@strehles.info>
2026-05-10 13:15:30 +0200

    add eq

--- crates/datex-core/src/types/type_definition/equality.rs
+++ crates/datex-core/src/types/type_definition/equality.rs
@@ -19,6 +19,60 @@ impl StructuralEq for TypeDefinition {
                 }
                 true
             }
+            (TypeDefinition::List(a), TypeDefinition::List(b)) => {
+                if a.len() != b.len() {
+                    return false;
+                }
+                for (item_a, item_b) in a.iter().zip(b.iter()) {
+                    if !item_a.structural_eq(item_b) {
+                        return false;
+                    }
+                }
+                true
+            }
+            (TypeDefinition::Nested(a), TypeDefinition::Nested(b)) => {
+                a.structural_eq(b)
+            }
+            (TypeDefinition::Shared(a), TypeDefinition::Shared(b)) => {
+                a.structural_eq(b)
+            }
+            (TypeDefinition::Callable(a), TypeDefinition::Callable(b)) => {
+                a.structural_eq(b)
+            }
+            (TypeDefinition::Core(c), TypeDefinition::Core(d)) => c == d,
+            (TypeDefinition::Map(a), TypeDefinition::Map(b)) => {
+                if a.len() != b.len() {
+                    return false;
+                }
+                for ((key_a, value_a), (key_b, value_b)) in
+                    a.iter().zip(b.iter())
+                {
+                    if !key_a.structural_eq(key_b)
+                        || !value_a.structural_eq(value_b)
+                    {
+                        return false;
+                    }
+                }
+                true
+            }
+            (
+                TypeDefinition::Range((a_start, a_end)),
+                TypeDefinition::Range((b_start, b_end)),
+            ) => a_start.structural_eq(b_start) && a_end.structural_eq(b_end),
+            (
+                TypeDefinition::Intersection(a),
+                TypeDefinition::Intersection(b),
+            ) => {
+                if a.len() != b.len() {
+                    return false;
+                }
+                for (item_a, item_b) in a.iter().zip(b.iter()) {
+                    if !item_a.structural_eq(item_b) {
+                        return false;
+                    }
+                }
+                true
+            }
             _ => false,
         }
     }
--- crates/datex-core/src/values/core_values/callable/equality.rs
+++ crates/datex-core/src/values/core_values/callable/equality.rs
@@ -1,9 +1,14 @@
 use crate::{
     traits::structural_eq::StructuralEq,
-    values::core_values::callable::Callable,
+    values::core_values::callable::{Callable, CallableSignature},
 };
 
 impl StructuralEq for Callable {
+    fn structural_eq(&self, other: &Self) -> bool {
+        self == other
+    }
+}
+impl StructuralEq for CallableSignature {
     fn structural_eq(&self, other: &Self) -> bool {
         self == other
     }
