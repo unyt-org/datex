12a9ccd28b16abe5560d4672fef6217efb977009
Benedikt Strehle <benedikt@unyt.org>
2026-05-29 18:13:25 +0200

    add serde_dif for BaseSharedValueContainer

--- crates/datex-core/src/shared_values/base_shared_value_container/serde_dif.rs
+++ crates/datex-core/src/shared_values/base_shared_value_container/serde_dif.rs
@@ -1,10 +1,14 @@
+use core::fmt;
 use crate::{
     dif::serde_context::SerdeContext,
     shared_values::base_shared_value_container::BaseSharedValueContainer,
     utils::serde_serialize_seed::{SerializeSeed, ValueWithSeed},
     values::value_container::ValueContainer,
 };
-use serde::{Serializer, de::DeserializeSeed, ser::SerializeStruct};
+use serde::{Serializer, de::DeserializeSeed, ser::SerializeStruct, Deserializer};
+use serde::de::{MapAccess, SeqAccess, Visitor};
+use serde::ser::SerializeSeq;
+use crate::types::r#type::Type;
 
 impl<'ctx> SerializeSeed for SerdeContext<'ctx, BaseSharedValueContainer> {
     type Value = BaseSharedValueContainer;
@@ -16,15 +20,20 @@ impl<'ctx> SerializeSeed for SerdeContext<'ctx, BaseSharedValueContainer> {
     ) -> Result<S::Ok, S::Error> {
         // serialize as struct
         let mut state =
-            serializer.serialize_struct("BaseSharedValueContainer", 1)?;
-        state.serialize_field("mutability", &value.mutability)?;
-        // TODO:
-        // state.serialize_field("allowed_type", &value.allowed_type)?;
-        state.serialize_field(
-            "value",
+            serializer.serialize_seq(Some(3))?;
+        
+        state.serialize_element(
             &ValueWithSeed::new(
                 &value.value_container,
                 self.cast::<ValueContainer>(),
+            ),
+        )?;
+
+        state.serialize_element(&value.mutability)?;
+        state.serialize_element(
+            &ValueWithSeed::new(
+                &value.allowed_type,
+                self.cast::<Type>(),
             ),
         )?;
         state.end()
@@ -38,8 +47,49 @@ impl<'de, 'ctx> DeserializeSeed<'de>
 
     fn deserialize<D: serde::Deserializer<'de>>(
         self,
-        _deserializer: D,
+        deserializer: D,
     ) -> Result<Self::Value, D::Error> {
-        todo!()
+        deserializer.deserialize_seq(self)
+    }
+}
+
+
+impl<'de, 'ctx> Visitor<'de> for SerdeContext<'ctx, BaseSharedValueContainer> {
+    type Value = BaseSharedValueContainer;
+
+    fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "a valid BaseSharedValueContainer")
+    }
+
+    fn visit_seq<A>(mut self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: SeqAccess<'de>,
+    {
+        let value_container = seq.next_element_seed(self.cast::<ValueContainer>())?.ok_or_else(|| {
+            serde::de::Error::invalid_length(0, &self)
+        })?;
+
+        let mutability = seq.next_element()?.ok_or_else(|| {
+            serde::de::Error::invalid_length(0, &self)
+        })?;
+
+
+        let allowed_type = seq.next_element_seed(self.cast::<Type>())?;
+
+        Ok(
+            if let Some(allowed_type) = allowed_type {
+                BaseSharedValueContainer::try_new(
+                    value_container,
+                    allowed_type,
+                    mutability,
+                ).map_err(|err| serde::de::Error::custom(format!("invalid BaseSharedValueContainer: {err}")))?
+            }
+            else {
+                BaseSharedValueContainer::new_with_inferred_allowed_type(
+                    value_container,
+                    mutability,
+                )
+            }
+        )
     }
 }
