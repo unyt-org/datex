e5884e3cc0329875bcf9efeb536be5d0a3b1c528
Benedikt Strehle <benedikt@unyt.org>
2026-06-22 00:12:33 +0200

    wip child iterators

--- crates/datex-core/src/values/core_values/range/child_iterator.rs
+++ crates/datex-core/src/values/core_values/range/child_iterator.rs
@@ -3,7 +3,7 @@ use crate::values::core_values::range::Range;
 use crate::values::value_container::ValueContainer;
 
 impl<'a> ChildIterator<'a> for Range {
-    fn iter_children(&'a self) -> impl Iterator<Item = &ValueContainer> + 'a {
+    fn iter_children(&self) -> impl Iterator<Item = &ValueContainer> {
         gen {
             yield self.start.as_ref();
             yield self.end.as_ref();
