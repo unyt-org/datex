a81fc6f922bab4e12f9286c95e4ae562f8998341
Jonas Strehle <jonas@strehles.info>
2026-07-02 20:01:44 +0200

    replace serde-value with serde-val (fork)

--- crates/datex-core/src/datex_proxy/serde_compat/mod.rs
+++ crates/datex-core/src/datex_proxy/serde_compat/mod.rs
@@ -7,43 +7,43 @@ use serde::{Deserialize, Serialize};
 
 mod serde_mapping;
 
-/// Converts a [Serialize] value into a [ValueContainer] by first converting it to a [serde_value::Value] and then deserializing it into a [ValueContainer].
+/// Converts a [Serialize] value into a [ValueContainer] by first converting it to a [serde_val::Value] and then deserializing it into a [ValueContainer].
 pub fn try_serde_to_value_container<T: Serialize>(
     value: T,
 ) -> Result<ValueContainer, TryToDatexValueError> {
-    let serde_val = serde_value::to_value(value)
+    let serde_val = serde_val::to_value(value)
         .map_err(|err| TryToDatexValueError(err.to_string()))?;
     serde_val
         .deserialize_into()
         .map_err(|err| TryToDatexValueError(err.to_string()))
 }
 
-/// Converts a [ValueContainer] into a rust value by first converting it to a [serde_value::Value] and then deserializing it into the target type.
+/// Converts a [ValueContainer] into a rust value by first converting it to a [serde_val::Value] and then deserializing it into the target type.
 pub fn try_serde_from_value_container<'de, T: Deserialize<'de>>(
     value: ValueContainer,
 ) -> Result<T, TryFromDatexValueError> {
-    let serde_val = serde_value::to_value(value)
+    let serde_val = serde_val::to_value(value)
         .map_err(|err| TryFromDatexValueError(err.to_string()))?;
     T::deserialize(serde_val)
         .map_err(|err| TryFromDatexValueError(err.to_string()))
 }
 
-/// Converts a [Serialize] value into a [Value] by first converting it to a [serde_value::Value] and then deserializing it into a [Value].
+/// Converts a [Serialize] value into a [Value] by first converting it to a [serde_val::Value] and then deserializing it into a [Value].
 pub fn try_serde_to_value<T: Serialize>(
     value: T,
 ) -> Result<Value, TryToDatexValueError> {
-    let serde_val = serde_value::to_value(value)
+    let serde_val = serde_val::to_value(value)
         .map_err(|err| TryToDatexValueError(err.to_string()))?;
     serde_val
         .deserialize_into()
         .map_err(|err| TryToDatexValueError(err.to_string()))
 }
 
-/// Converts a [Value] into a rust value by first converting it to a [serde_value::Value] and then deserializing it into the target type.
+/// Converts a [Value] into a rust value by first converting it to a [serde_val::Value] and then deserializing it into the target type.
 pub fn try_serde_from_value<'de, T: Deserialize<'de>>(
     value: Value,
 ) -> Result<T, TryFromDatexValueError> {
-    let serde_val = serde_value::to_value(value)
+    let serde_val = serde_val::to_value(value)
         .map_err(|err| TryFromDatexValueError(err.to_string()))?;
     T::deserialize(serde_val)
         .map_err(|err| TryFromDatexValueError(err.to_string()))
