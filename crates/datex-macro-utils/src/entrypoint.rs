841dade118668de32b824577eba6e24b4fff737c
Benedikt Strehle <benedikt@unyt.org>
2026-08-17 00:42:40 +0200

    fixes

--- crates/datex-macro-utils/src/entrypoint.rs
+++ crates/datex-macro-utils/src/entrypoint.rs
@@ -15,6 +15,7 @@ use syn::{
     Attribute, FnArg, Ident, ItemFn, LitStr, Pat, PatIdent, Token, Type,
     parse::{Parse, ParseStream},
 };
+use datex_core::datex_proxy::DatexValueContainerProxyInfallibleSerializeWithoutContext;
 
 #[derive(Debug)]
 pub struct ParsedAttributes {
@@ -254,7 +255,7 @@ pub fn get_arg_ident_and_type(
 fn compile_datex_config(config: RuntimeConfig) -> Vec<u8> {
     let (dxb, _) = compile_template(
         "?",
-        vec![Some(config.to_value_container())],
+        vec![Some(config.to_value_container_without_context())],
         CompileOptions::default(),
         // FIXME: stub runtime for now
         Runtime::stub(),
