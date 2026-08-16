921347e943b294e6af43b9e21c4e5c83d241889d
Jonas Strehle <jonas@strehles.info>
2026-08-16 11:12:20 +0200

    add impl_datex_callable for multi param callables

--- crates/datex-core/src/traits/callable.rs
+++ crates/datex-core/src/traits/callable.rs
@@ -8,6 +8,7 @@ use crate::{
         value_container::ValueContainer,
     },
 };
+use seq_macro::seq;
 
 pub trait IntoDatexCallable<Args, R> {
     /// Returns a vector of tuples containing the parameter names (if any) and their corresponding [Type]s.
@@ -18,82 +19,117 @@ pub trait IntoDatexCallable<Args, R> {
     fn invoke(&self, args: Vec<ValueContainer>) -> Result<R, CallableError>;
 }
 
-impl<F, R> IntoDatexCallable<(), R> for F
-where
-    F: Fn() -> R,
-{
-    fn parameters(
-        _memory: &mut SharedReferencesCache,
-    ) -> Vec<(Option<String>, Type)> {
-        vec![]
-    }
-
-    fn invoke(&self, args: Vec<ValueContainer>) -> Result<R, CallableError> {
-        if !args.is_empty() {
-            return Err(CallableError::InvalidSignature);
-        }
-        Ok(self())
-    }
+macro_rules! impl_datex_callable {
+    ($n:literal) => {
+        seq!(N in 0..$n {
+            impl<F, R, #(A~N,)*> IntoDatexCallable<(#(A~N,)*) , R> for F
+            where
+                F: Fn(#(A~N,)*) -> R,
+                #(
+                    A~N: DatexProxyTypes + TryFrom<ValueContainer>,
+                )*
+            {
+                fn parameters(
+                    #[allow(unused_variables)]
+                    memory: &mut SharedReferencesCache,
+                ) -> Vec<(Option<String>, Type)> {
+                    vec![#((None, A~N::datex_type(memory)),)*]
+                }
+                fn invoke(
+                    &self,
+                    args: Vec<ValueContainer>,
+                ) -> Result<R, CallableError> {
+                    // check that number of args is correct
+                    if args.len() != $n {
+                        return Err(CallableError::InvalidSignature);
+                    }
+                    #[allow(unused)]
+                    let mut args = args.into_iter();
+                    #(
+                        let arg~N: A~N = args
+                            .next()
+                            .unwrap()
+                            .try_into()
+                            .map_err(|_| CallableError::InvalidSignature)?;
+                    )*
+                    Ok(self(#(arg~N,)*))
+                }
+            }
+        });
+    };
 }
 
-impl<F, A, R> IntoDatexCallable<(A,), R> for F
-where
-    F: Fn(A) -> R,
-    A: DatexProxyTypes + TryFrom<ValueContainer>,
-{
-    fn parameters(
-        memory: &mut SharedReferencesCache,
-    ) -> Vec<(Option<String>, Type)> {
-        vec![(None, A::datex_type(memory))]
+impl_datex_callable!(0);
+impl_datex_callable!(1);
+impl_datex_callable!(2);
+impl_datex_callable!(3);
+impl_datex_callable!(4);
+impl_datex_callable!(5);
+impl_datex_callable!(6);
+impl_datex_callable!(7);
+impl_datex_callable!(8);
+impl_datex_callable!(9);
+impl_datex_callable!(10);
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        runtime::execution::ExecutionError,
+        traits::callable::IntoDatexCallable,
+        values::core_values::callable::error::CallableError,
+    };
+
+    #[test]
+    fn simple_call() {
+        let func = |x: u8, y: u8| x + y;
+        let result = func.invoke(vec![1u8.into(), 2u8.into()]).unwrap();
+        assert_eq!(result, 3);
     }
 
-    fn invoke(
-        &self,
-        mut args: Vec<ValueContainer>,
-    ) -> Result<R, CallableError> {
-        if args.len() != 1 {
-            return Err(CallableError::InvalidSignature);
-        }
+    #[test]
+    fn order() {
+        let func = |a: u8, b: u8, c: u8, d: u8, e: u8| vec![a, b, c, d, e];
+        let result = func
+            .invoke(vec![
+                1u8.into(),
+                2u8.into(),
+                3u8.into(),
+                4u8.into(),
+                5u8.into(),
+            ])
+            .unwrap();
+        assert_eq!(result, vec![1, 2, 3, 4, 5]);
+    }
 
-        let a = args
-            .pop()
-            .unwrap()
-            .try_into()
-            .map_err(|_| CallableError::InvalidSignature)?;
-        Ok(self(a))
+    #[test]
+    fn invalid_signature_args_count() {
+        let func = |x: u8, y: u8| x + y;
+        // only one instead of two args
+        let result = func.invoke(vec![1u8.into()]);
+        assert!(matches!(result, Err(CallableError::InvalidSignature)));
+    }
+
+    #[test]
+    fn invalid_signature_wrong_type() {
+        let func = |x: u8, y: u8| x + y;
+        // second arg is a text instead of u8
+        let result = func.invoke(vec![1u8.into(), "test".into()]);
+        assert!(matches!(result, Err(CallableError::InvalidSignature)));
+    }
+
+    #[test]
+    fn error_result() {
+        let func = |should_fail: bool| -> Result<u8, ExecutionError> {
+            if should_fail {
+                Err(ExecutionError::RequiresAsyncExecution)
+            } else {
+                Ok(42)
+            }
+        };
+        let result = func.invoke(vec![true.into()]).unwrap();
+        assert!(matches!(
+            result,
+            Err(ExecutionError::RequiresAsyncExecution)
+        ));
     }
 }
-impl<F, A, B, R> IntoDatexCallable<(A, B), R> for F
-where
-    F: Fn(A, B) -> R,
-    A: DatexProxyTypes + TryFrom<ValueContainer>,
-    B: DatexProxyTypes + TryFrom<ValueContainer>,
-{
-    fn parameters(
-        memory: &mut SharedReferencesCache,
-    ) -> Vec<(Option<String>, Type)> {
-        vec![(None, A::datex_type(memory)), (None, B::datex_type(memory))]
-    }
-
-    fn invoke(
-        &self,
-        mut args: Vec<ValueContainer>,
-    ) -> Result<R, CallableError> {
-        if args.len() != 2 {
-            return Err(CallableError::InvalidSignature);
-        }
-        let b = args
-            .pop()
-            .unwrap()
-            .try_into()
-            .map_err(|_| CallableError::InvalidSignature)?;
-        let a = args
-            .pop()
-            .unwrap()
-            .try_into()
-            .map_err(|_| CallableError::InvalidSignature)?;
-        Ok(self(a, b))
-    }
-}
-
-// TODO macro and support more than 2 params here
